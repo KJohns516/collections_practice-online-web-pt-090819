9e12db86b081a47f4bdb76b51b7c083a8410d64f
Kristan Johns <kristandjohns@gmail.com>
2019-09-21 01:35:11 +0000

    Automatically backed up by Learn

--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -5,6 +5,7 @@ describe 'collections practice' do
   # Question 1
   describe '#sort_array_asc' do
     it 'should return an array sorted in ascending order' do
+      yield
       expect(sort_array_asc([25, 7, 1])).to eq([1,7,25])
     puts sort_array_asc {"25,7,1"}
     end
